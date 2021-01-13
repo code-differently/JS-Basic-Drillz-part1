da2884c2253505f2f3a70a6bef60d4b85be8e3b8
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-12 22:18:20 -0500

    first commit

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,36 +1,73 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+        return "Hello World";
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     getPrefix(input){
-        return null;
+        let string = "";
+        string += input[0];
+        string += input[1];
+        string += input[2];
+        return string;
+
     }
 
     getSuffix(input){
-        return null;
+        let string = "";
+        string += input[input.length - 3];
+        string += input[input.length - 2];
+        string += input[input.length - 1];
+        return string;
     }
 
     getMiddleCharacter(input){
-        return null;
+        let middle = Math.floor(input.length/2);
+        if (input.length % 2 === 0) {
+            return input[middle-1] + input[middle];
+            }else {
+            return input[middle];
+        }
     }
 
     getFirstWord(input){
-        return null;
+        return input.split(" ")[0];
     }
     
     getSecondWord(spaceDelimnatedInput){
-        return null;
+        return spaceDelimnatedInput.split(" ")[1];
     }
     
     reverse(input){
-        return null;
+        let splitString = input.split("");
+        let reverseArray = splitString.reverse();
+        let joinArray = reverseArray.join("");
+        return joinArray;
     }
 }
 
 module.exports = StrangerStrings;
+
+// getPrefix(input){
+//     return input = input.substring(0, 3);
+// }
+
+// getSuffix(input){
+//     return input = input.substring(3, 7);
+// }
+
+// getMiddleCharacter(input){
+//     return input.substring(5, 4);
+// }
+
+// getFirstWord(input){
+//     return input = input.substring(0, 6);
+// }
+
+// getSecondWord(spaceDelimnatedInput){
+//     return "Clan";
+// }
