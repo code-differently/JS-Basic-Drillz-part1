d3b37bffb8723884a11e030d0fb256587b6aa23f
Amatullah Brown <amatullahbrown@Amatullahs-MBP.fios-router.home>
2021-01-13 14:52:08 -0500

    complete

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,35 +1,49 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+        return "Hello World";
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+        return firstSegment+secondSegment;
     }
 
     getPrefix(input){
-        return null;
+        //let output = input[0] + input[1] + input[2]; //can also use charAt() or input.substring(0,3)
+        return input.substring(0,3);
     }
 
     getSuffix(input){
-        return null;
+        // const total = input.length;
+        // let output = input[total-3] + input[total-2] + input[total-1];
+        return input.substring(input.length-3, input.length);
     }
 
     getMiddleCharacter(input){
-        return null;
+        const mid = Math.trunc(input.length / 2);
+        return input[mid];
     }
 
     getFirstWord(input){
-        return null;
+        let output = "";
+        for (let i=0; i<input.length; i++) {
+            if (input[i] === " ") {
+                return output;
+            }
+            output += input[i];
+        }
     }
     
     getSecondWord(spaceDelimnatedInput){
-        return null;
+        return spaceDelimnatedInput.split(" ")[1];
     }
     
     reverse(input){
-        return null;
+        let output = "";
+        for (let i=input.length-1; i>=0; i--) {
+            output+=input[i];
+        }
+        return output;
     }
 }
 
