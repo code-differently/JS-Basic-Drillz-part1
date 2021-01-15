5d6a44239fdf986a28f0e970b002ee9346f69b07
taylorhollins <74632541+taylorhollins@users.noreply.github.com>
2021-01-14 20:57:45 -0500

    complete

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,35 +1,64 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+        let helloWorld = "Hello World"
+        return helloWorld;
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     getPrefix(input){
-        return null;
+
+       /* let FirstLetter = input[0]
+        let SecondLetter = input [1]
+        let ThirdLetter = input [2]
+        return FirstLetter + SecondLetter + ThirdLetter; */
+        return input.substr(0,3);
     }
 
     getSuffix(input){
-        return null;
+
+        let FourthLetter = input[input.length - 3];
+        let FifthLetter = input[input.length - 2];
+        let lastLetter = input[input.length - 1];
+        return FourthLetter + FifthLetter + lastLetter;
+        
     }
 
     getMiddleCharacter(input){
-        return null;
+        let inputLength = input.length;
+        let middle = Math.floor(inputLength/2);
+        if (inputLength % 2 == 0){ 
+            // if even
+            return input[middle - 1] + input[middle];
+        } else {
+            //if odd
+         return input[middle];
+        }
     }
 
-    getFirstWord(input){
-        return null;
+    getFirstWord(inputValue){
+        let x = inputValue.split(" ");
+        let firstWord = x[0];
+        return firstWord;
     }
     
-    getSecondWord(spaceDelimnatedInput){
-        return null;
+    getSecondWord(inputValue){
+        let x = inputValue.split(" ");
+        let secondWord = x[1];
+        return secondWord;
     }
     
-    reverse(input){
-        return null;
+    reverse(inputValue){
+        let x = inputValue.split(""); 
+        // x = [w,u,t,a,n,g,c,l,a,n]
+        let rev = x.reverse();
+        // rev= n,a,l,c,g,n,a,t,u,w
+        let together = rev.join("");
+        // join = nalcgnatuw
+        return together;
     }
 }
 
