f843419ebed2d0ee6ded60b26c57ea51e1243cd9
lanayh3 <74631402+lanayh3@users.noreply.github.com>
2021-01-14 19:50:19 -0500

    input functions

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,35 +1,50 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+        return "Hello World";
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+        return firstSegment.concat(secondSegment);
     }
 
     getPrefix(input){
-        return null;
+        return input.substring(0, 3);
     }
 
     getSuffix(input){
-        return null;
+        return input.slice(input.length - 3);
     }
 
     getMiddleCharacter(input){
-        return null;
+        var position;
+        var length;
+
+        if(input.length % 2 == 1) {
+            position = input.length / 2;
+            length = 1;
+        } else {
+            position = input.length / 2 - 1;
+            length = 2;
+        }
+        return input.substring(position, position + length);
     }
 
     getFirstWord(input){
-        return null;
+        var firstWord = input.split(" ");
+        return firstWord[0];
     }
     
-    getSecondWord(spaceDelimnatedInput){
-        return null;
+    getSecondWord(input){
+        var secondWord = input.split(" ");
+        return secondWord[1];
     }
     
     reverse(input){
-        return null;
+        let splitString = input.split("");
+        let reverseArray = splitString.reverse();
+        let joinArray = reverseArray.join("");
+        return joinArray;
     }
 }
 
