ee30744b633105f8a6bd2968c41328b3689dde09
Lucas Kimanzi <lkimanzi@hotmail.com>
2021-01-20 18:18:32 -0500

    Completed MathUtil/StrangerStrings Class Exc.

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,36 +1,75 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+       let mystring = "Hello World"
+        return mystring
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+      firstSegment = "Wutang"
+      secondSegment = " Forever"
+        return firstSegment + secondSegment;
+      
     }
 
-    getPrefix(input){
-        return null;
+    getPrefix(mystring1){
+        mystring1="Wutang"
+        return mystring1.substring(0,3);
     }
 
-    getSuffix(input){
-        return null;
+    getSuffix(mystring2){
+        mystring2="Wutang"
+let last3char = mystring2.charAt (mystring2.length -3)
+let last2char = mystring2.charAt (mystring2.length -2)
+let lastchar = mystring2.charAt (mystring2.length -1)
+
+return  last3char + last2char + lastchar
+      
     }
 
-    getMiddleCharacter(input){
-        return null;
-    }
+    getMiddleCharacter(inputValue) {
+      //Figure out stringlength and to find out midpoint. Declare the variables for this
+              let midpoint;
+              let length;
+      //for even #characters -returning 1 character. NOTE this is for odd string length
+              if(inputValue.length % 2 == 1) {
+                  midpoint = inputValue.length / 2;
+                  length = 1;
+      }
+      //for odd #characters -returning 2 char.  NOTE this is for even string length
+               else {
+                  midpoint = inputValue.length / 2 - 1;
+                  length = 2;
+              }
+      
+              return inputValue.substring(midpoint, midpoint + length)
+          }
+      
+  
+          getFirstWord(inputValue){
+            inputValue = "Wutang Clan";
+            let splitinput = inputValue.split(" ")
+            let firstword = inputValue.split(" ")[0]
+            return firstword;
+              }
+              
+          
+  
+  getSecondWord(spaceDelimnatedInput){
+      return null;
+  }
 
-    getFirstWord(input){
-        return null;
-    }
-    
-    getSecondWord(spaceDelimnatedInput){
-        return null;
-    }
-    
-    reverse(input){
-        return null;
-    }
+  getSecondWord(inputValue){
+    inputValue = "Wutang Clan";
+    let splitinput = inputValue.split(" ")
+    let secondword = inputValue.split(" ")[1]
+    return secondword;
+      }
+  
+      reverse(inputValue){
+      inputValue = "WutangClan"
+      return inputValue.split("").reverse().join("");
+          }
+  
 }
-
 module.exports = StrangerStrings;
